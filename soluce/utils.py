14dbcc85e937e89a48a0c89dc02b9cd23ef1cc1b
iTrix <marwan.mezroui.pro@gmail.com>
2020-12-28 08:15:22 +0100

    add functions declaration & docstring

---
+++ soluce/utils.py
@@ -0,0 +1,32 @@
+##########################################
+# author: MEZROUI Marwan, BULTEZ Victor
+# created: 28/12/2020
+# modified: 28/12/2020
+##########################################
+
+def print_board(size, board):
+  """Display the board.
+
+  Args:
+      size (int): size of the board
+      board (int): 2 dimensional array representing the board
+  """
+  pass
+
+def can_t_attack(size, board):
+  """Check none queen can be attacked by another.
+
+  Args:
+      size (int): size of the board
+      board (int): 2 dimensional array representing the board
+  """
+  pass
+
+def is_soluce(size, board):
+  """Check solution has been found.
+
+  Args:
+      size (int): size of the board
+      board (int): 2 dimensional array representing the board
+  """
+  pass
