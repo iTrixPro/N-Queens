fd3c27b31a391b324531bd31a19c9fbb66a6515f
iTrix <marwan.mezroui.pro@gmail.com>
2021-01-06 18:14:38 +0100

    move utilities functions from solving to utils

--- soluce/solving.py
+++ soluce/solving.py
@@ -1,30 +1,77 @@
 ##########################################
 # author: MEZROUI Marwan, BULTEZ Victor
-# created: 18/12/2020
-# modified: 05/01/2020
+# modified: 06/01/2020
+# see: utils.py
+# description: Contains the different algorithms used to resolve the N-Queen problem
 ##########################################
 
-def backtracking(board, size):
+from soluce.utils import *
+
+def backtracking(board, column, size):
     """Algo to solve the problem. Deprecated for a size superior at 20.
 
     Args:
-        board (Board)
+        board (array)
         size (int): size of the board
 
     Returns:
-        [Board]: board with the solution
+        [array]: final board
         [boolean]: success to find a solution
     """
-    nb_queen = board.getNumberOfQueen()
-    if nb_queen == size:
+
+    if column == size:
         return board, True
 
     for row in range(size):
-        if board.queenCanTattack(row, nb_queen):
-            board.add(row, nb_queen)
-            board, solved = backtracking(board, size)
+        if queenCanTattack(board, size, row, column):
+            board[row][column] = 1
+            board, solved = backtracking(board, column + 1, size)
+
             if solved:
                 return board, solved
-            board.remove(row, nb_queen)
+
+            board[row][column] = 0
+
+    return board, False
+
+def branchAndBound(board,
+                   column,
+                   size,
+                   oblique,
+                   oblique_inv,
+                   ctrl_oblique,
+                   ctrl_obliqueinv):
+    """[summary]
+
+    Args:
+        board (array)
+        column (int): number of queen, will be the column index
+        size (int): size of the board
+        oblique ([type]): [description]
+        oblique_inv ([type]): [description]
+        ctrl_oblique ([type]): [description]
+        ctrl_obliqueinv ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+
+    if column >= size:
+        return board, True
+
+    for row in range(size):
+        if isSafe(row, column, oblique, oblique_inv, ctrl_oblique, ctrl_obliqueinv):
+            board[row][column] = 1
+            ctrl_oblique[oblique[row][column]] = True
+            ctrl_obliqueinv[oblique_inv[row][column]] = True
+
+            board, solved = branchAndBound(board, column + 1, size, oblique, oblique_inv, ctrl_oblique, ctrl_obliqueinv)
+
+            if solved:
+                return board, solved
+
+            board[row][column] = 0
+            ctrl_oblique[oblique[row][column]] = False
+            ctrl_obliqueinv[oblique_inv[row][column]] = False
 
     return board, False
--- soluce/utils.py
+++ soluce/utils.py
@@ -1,63 +1,112 @@
 ##########################################
 # author: MEZROUI Marwan, BULTEZ Victor
-# created: 28/12/2020
-# modified: 05/01/2021
+# modified: 06/01/2020
+# descritpion: contains all the utilities functions needed by solving.py and nqueen.py
 ##########################################
 
-from soluce.board import Board
-from soluce.solving import *
-
-def print_board(size, board):
-  """Display the board.
+def queenCanTattack(board, size, row, column):
+  """Check if the new queen will not be able to attack an other one.
 
   Args:
+      board (array): board on which the queen will be
       size (int): size of the board
-      board (array): 2 dimensional array containing the content of the board
-  """
-
-  board = Board(board, size)
-  print(board)
-
-def can_t_attack(size, board):
-  """Check that none of the queens can attack each others.
-
-  Args:
-      size (int): size of the board
-      board (array): 2 dimensional array containing the content of the board
+      row (int): row position on the board
+      column (int): column position on the board
 
   Returns:
-      [boolean]: True, if none of them can attack
+      [boolean]: True, if unable to attack
   """
-  board = Board(board, size)
-  return board.queensCanTattack()
 
-def is_soluce(size, board):
-  """Check solution has been found.
+  can_t_attack = True
+
+  # check cardinals
+  for idx_row in range(size):
+      if idx_row != row and board[idx_row][column] == 1:
+          return not can_t_attack
+
+  for idx_column in range(size):
+      if idx_column != column and board[row][idx_column] == 1:
+          return not can_t_attack
+
+  # check diagonals
+  for idx_row, idx_column in zip(range(row - 1, -1, -1), range(column + 1, size)):
+      if board[idx_row][idx_column] == 1:
+          return not can_t_attack
+
+  for idx_row, idx_column in zip(range(row + 1, size), range(column + 1, size)):
+      if board[idx_row][idx_column] == 1:
+          return not can_t_attack
+
+  for idx_row, idx_column in zip(range(row - 1, -1, -1), range(column - 1, -1, -1)):
+      if board[idx_row][idx_column] == 1:
+          return not can_t_attack
+
+  for idx_row, idx_column in zip(range(row + 1, size), range(column - 1, -1, -1)):
+      if board[idx_row][idx_column] == 1:
+          return not can_t_attack
+
+  return can_t_attack
+
+def isSafe(row, col, oblique, oblique_inv, ctrl_oblique, ctrl_obliqueinv):
+  """[summary]
 
   Args:
-      size (int): size of the board
-      board (array): 2 dimensional array containing the content of the board
-  """
-  if not isinstance(board, Board):
-    board = Board(board, size)
-
-  is_soluce = True if board.getNumberOfQueen() == size else False
-
-  if(is_soluce):
-    is_soluce = board.queensCanTattack()
-
-  return is_soluce, board.getNumberOfQueen()
-
-def solve_n_queen_small(size, board):
-  """Solve the problem for a small board.
-
-  Args:
-      size (int): size of the board
-      board (array): 2 dimensional array containing the content of the board
+      row ([type]): [description]
+      col ([type]): [description]
+      oblique ([type]): [description]
+      oblique_inv ([type]): [description]
+      ctrl_oblique ([type]): [description]
+      ctrl_obliqueinv ([type]): [description]
 
   Returns:
-      [Board]: Board object
-      [boolean]: success or not
+      [type]: [description]
   """
-  board = Board(board, size)
-  return backtracking(board, size)
+  if ctrl_oblique[oblique[row][col]] or ctrl_obliqueinv[oblique_inv[row][col]] or ctrl_obliqueinv[row]:
+      return False
+  return True
+
+def initCtrl(size):
+  """Init...
+
+  Args:
+      size (int): size of the board
+
+  Returns:
+      [type]: [description]
+  """
+  x = 2 * size - 1
+  ctrl_oblique = [False] * x
+  ctrl_obliqueinv = [False] * x
+
+  return ctrl_oblique, ctrl_obliqueinv
+
+def initObliques(size):
+  """Init...
+
+  Args:
+      size (int): size of the board
+
+  Returns:
+      [type]: [description]
+  """
+  oblique = generateArray(size)
+  oblique_inv = generateArray(size)
+
+  for row in range(size):
+      for column in range(size):
+          oblique[row][column] = row + column
+          oblique_inv[row][column] = row - column + (size -1)
+
+  return oblique, oblique_inv
+
+def generateArray(size):
+  """Return a 2D array full of 0.
+
+  Args:
+      size (int): number of row and number of column
+
+  Returns:
+      [array]: 2D array full of 0
+  """
+
+  return [[0 for x in range(size)] for y in range(size)]
