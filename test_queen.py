941ca7e8b68f722aaa9efdd2bdec02af8dd6ee60
iTrix <marwan.mezroui.pro@gmail.com>
2021-01-11 03:10:42 +0100

    modify to respect PEP8

--- test_queen.py
+++ test_queen.py
@@ -1,17 +1,16 @@
 ##########################################
 # author: MEZROUI Marwan, BULTEZ Victor
-# created: 01/01/2021
-# modified: 01/05/2021
+# modified: 11/01/2021
 ##########################################
-
 from soluce.queen import Queen
 
+
 def test_getPos():
-  queen = Queen(5,8)
-  assert queen.getPosX() == 5
-  assert queen.getPosY() == 8
+    queen = Queen(5, 8)
+    assert queen.getPosX() == 5
+    assert queen.getPosY() == 8
 
 
 def test_getSymbol():
-  queen = Queen(0,0)
-  assert queen.getSymbol() == 1
+    queen = Queen(0, 0)
+    assert queen.getSymbol() == 1
