1a748c47b68b0bd037da473dca057372d7268b34
iTrix <marwan.mezroui.pro@gmail.com>
2021-01-11 03:36:58 +0100

    modify to respect PEP8

--- soluce/board.py
+++ soluce/board.py
@@ -1,177 +1,188 @@
 ##########################################
 # author: MEZROUI Marwan, BULTEZ Victor
-# modified: 06/01/2020
+# modified: 11/01/2020
 # see: queen.py
 ##########################################
 
 from soluce.queen import Queen
 
-class Board:
-  """A class to represent a board.
 
-  Attributes:
+class Board:
+    """A class to represent a board.
+
+    Attributes:
         content (array) : content of the board
         size (int) : size of the board
         has_queen (boolean) : contains a queen
-  Methods:
+    Methods:
         getSize() - return the size of the board
-        queensCanTattack() - check that none of the queens can attack each others
+        queensCanTattack() - check that none of the queens
+        can attack each others
         getNumberOfQueen() - get the number of queen
-  """
-
-  def __init__(self, content, size):
-    """Constructs all the necessary attributes for the board object.
-
-    Args:
-        content (array) : starting content of the board
-        size (int) : size of the board
-        has_queen (boolean) : contains a queen
-        number_of_queen (int)
     """
 
-    self.content = None
-    self.size = size
-    self.has_queen = False
-    self.number_of_queen = 0
-    self._setContent(content)
+    def __init__(self, content, size):
+        """Constructs all the necessary attributes for the board object.
 
-  def _setContent(self, content):
-    """Setter for the content attribute, will put Queen objects at the place of the 1.
+        Args:
+          content (array) : starting content of the board
+          size (int) : size of the board
+          has_queen (boolean) : contains a queen
+          number_of_queen (int)
+        """
 
-    Args:
-        content (array): content of the board
+        self.content = None
+        self.size = size
+        self.has_queen = False
+        self.number_of_queen = 0
+        self._setContent(content)
 
-    Returns:
-        [array]: final starting content with the 1 remplace by Queen object
-    """
+    def _setContent(self, content):
+        """Setter for the content attribute,
+        will put Queen objects at the place of the 1.
 
-    update = False
-    for row in range(self.size):
-      for column in range(self.size):
-        if content[row][column] == 1:
-          content[row][column] = Queen(row, column)
-          self.number_of_queen += 1
+        Args:
+          content (array): content of the board
 
-          if(not update):
-            self.has_queen = True
-            update = True
+        Returns:
+          [array]: final starting content with the 1 remplace by Queen object
+        """
 
-    self.content = content
+        update = False
+        for row in range(self.size):
+            for column in range(self.size):
+                if content[row][column] == 1:
+                    content[row][column] = Queen(row, column)
+                    self.number_of_queen += 1
 
-  def getSize(self):
-    """Get the size of the board.
+                    if(not update):
+                        self.has_queen = True
+                        update = True
 
-    Returns:
-        [int]: size of the board, knowing number of rows = number of columns
-    """
+        self.content = content
 
-    return self.size
+    def getSize(self):
+        """Get the size of the board.
 
-  def getNumberOfQueen(self):
-    """Get the number of queen on the board
+        Returns:
+          [int]: size of the board, knowing number of rows = number of columns
+        """
 
-    Returns:
-        [int]: number of queen
-    """
+        return self.size
 
-    return self.number_of_queen
+    def getNumberOfQueen(self):
+        """Get the number of queen on the board
 
-  def queensCanTattack(self):
-    """Check that none of the Queen objects on the board can attack each others.
+        Returns:
+          [int]: number of queen
+        """
 
-    Returns:
-        [boolean]: True if none of the queens can attack each others
-    """
+        return self.number_of_queen
 
-    can_t_attack = True
-    if(not self.has_queen):
-      return can_t_attack
+    def queensCanTattack(self):
+        """Check that none of the Queen objects on the board can attack each others.
 
-    queens = self._getQueens()
-    it = 0
+        Returns:
+          [boolean]: True if none of the queens can attack each others
+        """
 
-    for attacker in queens:
-      can_t_attack = self._checkDiagonals(attacker)
+        can_t_attack = True
+        if(not self.has_queen):
+            return can_t_attack
 
-      if (can_t_attack):
-        it += 1
-        for victim in queens[it:len(queens)]:
-          if attacker.getPosX() == victim.getPosX() or attacker.getPosY() == victim.getPosY():
-            return not can_t_attack
+        queens = self._getQueens()
+        it = 0
 
-    return can_t_attack
+        for attacker in queens:
+            can_t_attack = self._checkDiagonals(attacker)
 
-  def _getQueens(self):
-    """Get all the queens on the board.
+            if (can_t_attack):
+                it += 1
+                for victim in queens[it:len(queens)]:
+                    if attacker.getPosX() == victim.getPosX() or \
+                       attacker.getPosY() == victim.getPosY():
+                        return not can_t_attack
 
-    Returns:
-        [array]: contains all the Queen objects present on the board
-    """
-    queens = []
-    nb_queen_found = 0
-    for row in self.content:
-      for column in row:
-        if isinstance(column, Queen):
-          queens.append(column)
-          nb_queen_found += 1
-          if nb_queen_found == self.number_of_queen:
-            break
+        return can_t_attack
 
-    return queens
+    def _getQueens(self):
+        """Get all the queens on the board.
 
-  def _checkDiagonals(self, queen):
-    """Check if the queen can't attack an other one on the diagonals
+        Returns:
+          [array]: contains all the Queen objects present on the board
+        """
 
-    Args:
-        queen (Queen): instance of queen on the board
+        queens = []
+        nb_queen_found = 0
+        for row in self.content:
+            for column in row:
+                if isinstance(column, Queen):
+                    queens.append(column)
+                    nb_queen_found += 1
+                    if nb_queen_found == self.number_of_queen:
+                        break
 
-    Returns:
-        [boolean]: False if can attack an other queen
-    """
+        return queens
 
-    for row, column in zip(range(queen.getPosY() - 1, -1, -1), range(queen.getPosX() + 1, self.size)):
-        if isinstance(self.content[row][column], Queen):
-            return False
+    def _checkDiagonals(self, queen):
+        """Check if the queen can't attack an other one on the diagonals
 
-    for row, column in zip(range(queen.getPosY() + 1, self.size), range(queen.getPosX() + 1, self.size)):
-        if isinstance(self.content[row][column], Queen):
-            return False
+        Args:
+          queen (Queen): instance of queen on the board
 
-    for row, column in zip(range(queen.getPosY() - 1, -1, -1), range(queen.getPosX() - 1, -1, -1)):
-        if isinstance(self.content[row][column], Queen):
-            return False
+        Returns:
+            [boolean]: False if can attack an other queen
+        """
 
-    for row, column in zip(range(queen.getPosY() + 1, self.size), range(queen.getPosX() - 1, -1, -1)):
-        if isinstance(self.content[row][column], Queen):
-            return False
+        for row, column in zip(range(queen.getPosY() - 1, -1, -1),
+                               range(queen.getPosX() + 1, self.size)):
+            if isinstance(self.content[row][column], Queen):
+                return False
 
-    return True
+        for row, column in zip(range(queen.getPosY() + 1, self.size),
+                               range(queen.getPosX() + 1, self.size)):
+            if isinstance(self.content[row][column], Queen):
+                return False
 
-  def __repr__(self):
-    """Representation of the board.
+        for row, column in zip(range(queen.getPosY() - 1, -1, -1),
+                               range(queen.getPosX() - 1, -1, -1)):
+            if isinstance(self.content[row][column], Queen):
+                return False
 
-    Returns:
-        [string]: return the content of the board as a string
-    """
-    return self._print()
+        for row, column in zip(range(queen.getPosY() + 1, self.size),
+                               range(queen.getPosX() - 1, -1, -1)):
+            if isinstance(self.content[row][column], Queen):
+                return False
 
-  def _print(self):
-    """Return the content of the booard as a string.
+        return True
 
-    Returns:
-        [string]: content of the board
-    """
-    display = ''
+    def __repr__(self):
+        """Representation of the board.
 
-    for idx_row, row in enumerate(self.content):
-      for idx_col, column in enumerate(row):
-        display += ' '
-        if (isinstance(column, Queen)):
-          display += column.getSymbol()
-        else:
-          display += str(column)
+        Returns:
+          [string]: return the content of the board as a string
+        """
 
-        if idx_col == self.size - 1 and idx_row != self.size - 1:
-          display += '\n'
+        return self._print()
 
-    return display
+    def _print(self):
+        """Return the content of the board as a string.
+
+        Returns:
+          [string]: content of the board
+        """
+
+        display = ''
+
+        for idx_row, row in enumerate(self.content):
+            for idx_col, column in enumerate(row):
+                display += ' '
+                if (isinstance(column, Queen)):
+                    display += column.getSymbol()
+                else:
+                    display += str(column)
+
+                if idx_col == self.size - 1 and idx_row != self.size - 1:
+                    display += '\n'
+
+        return display
